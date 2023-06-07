a82151260f8dac2a038253ecc71b7f4323ad7d6b
Julio Orellana <coppro.cman@proton.me>
2023-06-07 16:05:22 +0200

    :hammer: Improve Email Shemas

--- app/schemas/emails.py
+++ app/schemas/emails.py
@@ -1,32 +1,28 @@
 from email_validator import  EmailNotValidError, validate_email 
 from pydantic import BaseModel, EmailStr, validator
+
+def email_validation(email):
+    valid_email_format = email.lower()
+    try:
+        email_object = validate_email(valid_email_format)
+        return email_object.email
+    except EmailNotValidError as errorMsg:
+        raise errorMsg
+
     
 class EmailContent(BaseModel):
     email: EmailStr
     subject: str
     content: str
-    
     @validator("email")
-    def validate_email(cls, value: str):
-        valid_email_format = value.lower()
-        try:
-            email_object = validate_email(valid_email_format)
-            return email_object.email
-        except EmailNotValidError as errorMsg:
-            raise errorMsg
-        
+    def email_in(cls, value):
+        email_validation(value)
 
 class EmailValidation(BaseModel):
     email: EmailStr
     subject: str
     token: str
-    
     @validator("email")
-    def validate_email(cls, value: str):
-        valid_email_format = value.lower()
-        try:
-            email_object = validate_email(valid_email_format)
-            return email_object.email
-        except EmailNotValidError as errorMsg:
-            raise errorMsg
+    def email_in(cls, value):
+        email_validation(value)
         
