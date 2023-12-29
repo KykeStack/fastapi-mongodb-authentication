e09e1be4d6cfb42555a8007f3be80021309d63fe
Julio Orellana <coppro.cman@proton.me>
2023-12-30 00:14:37 +0100

    Update Assemble Magic Token Endpoints

--- api/api_v1/endpoints/validate.py
+++ api/api_v1/endpoints/validate.py
@@ -10,7 +10,7 @@ from pymongo.collection import Collection
 from pymongo.database import Database
 from bson.objectid import ObjectId
 
-from jose import jwt, JWTError
+from jose import jwt
 import json
 
 INVALID_IDENTIFIER = 'Please, check identifier.'
@@ -18,8 +18,7 @@ INVALID_JWT = 'Of course! You shall not pass!'
 
 JWT_SECRET =  settings.SECRET_KEY
 JWT_ALGORITHM = settings.JWT_ALGORITHM
-
-# ----- Section To be deleted just for testing purposes -----    
+UNAUTHORIZED_MESSAGE = HTTPException(status_code=401, detail="Could not Validate Credentials")
 
 def magic_get_db() -> Union[Collection, Database]:
     try:
@@ -48,44 +47,40 @@ async def request(url: str, json_data: dict, headers: dict) -> Union[dict, HTTPE
                 data = await responce.read()
                 hashrate = json.loads(data)
         except HTTPException as e:
-            raise print(e)
+            raise UNAUTHORIZED_MESSAGE
         return hashrate
         
 @router.get('/magic-login', status_code=200)
 def validate(magic: str, collection: Collection = Depends(magic_get_db)):
     magic_login_url =  f"{settings.SERVER_HOST}{settings.API_V1_STR}/login/claim"
+    
     try:
         payload = jwt.decode(magic, JWT_SECRET, algorithms=[JWT_ALGORITHM])     
-    except JWTError:
-        raise HTTPException(status_code=400, detail="Could not Validate Credentials")
-    try:
         user: dict = collection.find_one({'foreignId': ObjectId(payload.get('sub'))}) 
     except Exception as error:
-        raise HTTPException(status_code=400, detail="Could not Validate Credentials")
-    if user == None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise UNAUTHORIZED_MESSAGE
+
+    if not user:
+        raise UNAUTHORIZED_MESSAGE
+    
     json_data = {'claim': user.get('claimToken')}
     bearer_token = {'Authorization': f'Bearer {magic}'}
-    get_access_data = asyncio.run(request(url=magic_login_url, json_data=json_data, headers=bearer_token))
-    return get_access_data
-        
+
+    return asyncio.run(request(url=magic_login_url, json_data=json_data, headers=bearer_token))
+    
 
 @router.get('/reset-password', status_code=200)
 def validate(token: str, password: Optional[str] = 'password123', collection: Collection = Depends(password_get_db)):
     reset_password_url = f"{settings.SERVER_HOST}{settings.API_V1_STR}/login/reset"  
     try:
         payload = jwt.decode(token=token, key=JWT_SECRET, algorithms=[JWT_ALGORITHM])     
-    except JWTError:
-        raise HTTPException(status_code=400, detail="Could not Validate Credentials")
-    try:
         user: dict = collection.find_one({'foreignId': ObjectId(payload.get('sub'))}) 
     except Exception as error:
-        raise HTTPException(status_code=400, detail="Could not Validate Credentials")
-    if user == None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise UNAUTHORIZED_MESSAGE
+    
+    if not user:
+        raise UNAUTHORIZED_MESSAGE
+    
     json_data = {'new_password': password, 'claim': user.get('claimToken')}
     bearer_token = {'Authorization': f'Bearer {token}'}
-    get_access_data = asyncio.run(request(url=reset_password_url, json_data=json_data, headers=bearer_token))
-    return get_access_data
-        
-# ----- End Section -----
+    return asyncio.run(request(url=reset_password_url, json_data=json_data, headers=bearer_token))
