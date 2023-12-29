1da7c4ff14917d8f8d07e9ee6e9aadfb2fb51087
Julio Orellana <coppro.cman@proton.me>
2023-12-30 00:10:26 +0100

    Refactor CRUD Tokens Module

--- crud/tokens.py
+++ crud/tokens.py
@@ -8,7 +8,7 @@ from typing import Optional
 from pymongo.collection import Collection
 from bson.objectid import ObjectId
 
-mssg = HTTPException(
+NOT_FOUND_MESSAGE = HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Service Unavailable"
         )
@@ -24,25 +24,17 @@ def set_db_tokens(
         update_content = UpdateMagicData(claimToken=token, updatedAt=datetime.now())
         responce = collection.update_one({"foreignId": id}, {"$set": {**update_content.dict()}})
     except Exception as error:
-        error_handler = FunctionStatus(
-            functionName='set_db_tokens', status=False, section=0, message=error)
-        print(error_handler)
-        raise mssg
+        raise NOT_FOUND_MESSAGE
+    
     if responce.modified_count == 0:
         new_content = MagicData(foreignId=id, claimToken=token)
         try:
             responce = collection.insert_one({**new_content.dict()})
         except Exception as error:
-            error_handler = FunctionStatus(
-                functionName='set_db_tokens', status=False, section=1, message=error)
-            print(error_handler)
-            raise mssg
+            raise NOT_FOUND_MESSAGE
+        
     if not responce.acknowledged:
-        error_handler = FunctionStatus(
-            functionName='set_db_tokens', status=False, section=2, 
-            message=f"Error: acknowledged -> {responce.acknowledged}")
-        print(error_handler)
-        raise mssg
+        raise NOT_FOUND_MESSAGE
     
 def verify_token(
     *, 
@@ -53,19 +45,16 @@ def verify_token(
     try: 
         db: dict = collection.find_one({"foreignId": id})
     except Exception as error:
-        error_handler = FunctionStatus(
-            functionName="validate_magic_link", status=False, section=2, message=error)
-        print(error_handler)
-        raise mssg
-    if db == None:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials",
-        )  
+        raise NOT_FOUND_MESSAGE
+    
+    if not db:
+        raise NOT_FOUND_MESSAGE
+    
     if db.get('claimToken') != claim:
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid Token",
         )
+
 if __name__ == "__main__":
     ...
